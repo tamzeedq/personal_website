e228fd737da797354aa31acf7325827bf98ab7ea
tamzeedq <74509993+tamzeedq@users.noreply.github.com>
2023-07-17 22:29:22 -0700

    setup footer and pages

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,25 +1,29 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
         <div className="flex-1">
-            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            <Link href='/' className="btn btn-ghost normal-case text-xl">Tamzeed Quazi</Link>
         </div>
         <div className="flex-none">
             <ul className="menu menu-horizontal px-1">
-            <li><a>Link</a></li>
-            <li>
-                <details>
-                <summary>
-                    Parent
-                </summary>
-                <ul className="p-2 bg-base-100">
-                    <li><a>Link 1</a></li>
-                    <li><a>Link 2</a></li>
-                </ul>
-                </details>
-            </li>
+                <li><Link href='/about'>About</Link></li>
+                <li><Link href='/projects'>Projects</Link></li>
+                <li><Link href='/resume'>Resume</Link></li>
+                <li><Link href='/contact'>Contact</Link></li>
+                {/* <li>
+                    <details>
+                    <summary>
+                        Parent
+                    </summary>
+                    <ul className="p-2 bg-base-100">
+                        <li><a>Link 1</a></li>
+                        <li><a>Link 2</a></li>
+                    </ul>
+                    </details>
+                </li> */}
             </ul>
         </div>
     </div>
