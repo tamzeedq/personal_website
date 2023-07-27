8786719298e2d85e96c4c2ee1f69d86bca121204
tamzeedq <74509993+tamzeedq@users.noreply.github.com>
2023-07-26 23:11:42 -0700

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,13 +11,13 @@ const About = () => {
         <h1 className='text-7xl'>About Me</h1>
         <hr />
         <br />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint provident, quidem quod illo vero minima reiciendis ex officia fugiat ut odio blanditiis accusantium quae saepe aliquam alias perspiciatis iste laudantium enim repellendus hic quas modi corporis facere. Dolore ipsum corporis deserunt, autem iste architecto vel veniam inventore dolores ea quisquam!</p>
+        <p>Hi! My name is Tamzeed Quazi. I am in my third year at the University of British Columbia pursuing a combined major in Computer Science and Statistics. Currently, I am a front-end developer for UBC MINT, and a full-stack developer and software engineer at my co-op. In the future, I hope to pursue a career in machine learning.</p>
         <br />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint provident, quidem quod illo vero minima reiciendis ex officia fugiat ut odio blanditiis accusantium quae saepe aliquam alias perspiciatis iste laudantium enim repellendus hic quas modi corporis facere. Dolore ipsum corporis deserunt, autem iste architecto vel veniam inventore dolores ea quisquam!</p>
+        <p>I enjoy watching movies, playing various sports, going to the gym, playing chess, gaming, and lately have been tinkering with my new Arduino starter kit. Check out my resume to see more of the professional experience I have!</p>
         <br />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint provident, quidem quod illo vero minima reiciendis ex officia fugiat ut odio blanditiis accusantium quae saepe aliquam alias perspiciatis iste laudantium enim repellendus hic quas modi corporis facere. Dolore ipsum corporis deserunt, autem iste architecto vel veniam inventore dolores ea quisquam!</p>
-        <br />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint provident, quidem quod illo vero minima reiciendis ex officia fugiat ut odio blanditiis accusantium quae saepe aliquam alias perspiciatis iste laudantium enim repellendus hic quas modi corporis facere. Dolore ipsum corporis deserunt, autem iste architecto vel veniam inventore dolores ea quisquam!</p>
+        <p>Feel free to check out my socials and get in touch!</p>
+        {/* <br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint provident, quidem quod illo vero minima reiciendis ex officia fugiat ut odio blanditiis accusantium quae saepe aliquam alias perspiciatis iste laudantium enim repellendus hic quas modi corporis facere. Dolore ipsum corporis deserunt, autem iste architecto vel veniam inventore dolores ea quisquam!</p> */}
       </div>
     </div>
   );
