b7d1f635e2a8ead7b87f2503c30a829c10255701
tamzeedq <74509993+tamzeedq@users.noreply.github.com>
2023-09-09 21:22:54 -0700

    homepage update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { BsCpuFill, BsFillLightningChargeFill, BsArrowDownShort, BsArrowUpShort } from 'react-icons/bs';
-import { FiMonitor } from 'react-icons/fi';
-import { BiCodeBlock, BiGitBranch } from 'react-icons/bi';
 import HoverCard from './components/hoverCard';
 import Model from './components/model';
+import { FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { TypeAnimation } from 'react-type-animation';
 
 export default function Home() {
   // const router = useRouter();
@@ -26,8 +24,42 @@ export default function Home() {
       <div className='relative'>
         <Model></Model>
         <div className='absolute bottom-[50%] w-full flex flex-col justify-center items-center bg-transparent'>
-          <h1 className='text-7xl drop-shadow-xl text-white'>TAMZEED QUAZI</h1>
-          <h5 className='text-xl drop-shadow text-zinc-100'>Student Developer</h5>
+          <h1 className='md:text-7xl sm:text-5xl text-3xl drop-shadow-xl text-white'>TAMZEED QUAZI</h1>
+          <TypeAnimation
+            sequence={[
+              'Software Developer', 
+              1000,
+              'Computer Science student at UBC', 
+              1000, 
+              'Passionate Learner',
+              1000,  
+            ]}
+            wrapper="span"
+            repeat={Infinity}
+            style={{ fontSize: '1.25rem', display: 'inline-block' , color: 'rgb(248 113 113)'}}
+          />
+          <div className='flex items-center justify-center gap-x-2 pt-2'>
+            <button className='hover:scale-105 hover:text-amber-300'>
+              <Link href="https://github.com/tamzeedq">
+                <FaGithub></FaGithub>
+              </Link>
+            </button>
+            <button className='hover:scale-105 hover:text-indigo-500'>
+              <Link href="https://www.instagram.com/tamzeed.q/">
+                <FaInstagram></FaInstagram>
+              </Link>
+            </button>
+            <button className='hover:scale-105 hover:text-teal-200'>
+              <Link href="https://www.linkedin.com/in/tamzeedquazi/">
+                <FaLinkedin></FaLinkedin>
+              </Link>
+            </button>
+            <button className='hover:scale-105 hover:text-red-200'>
+              <Link href="mailto:tamzeed.q@gmail.com">
+                <HiOutlineMail></HiOutlineMail>
+              </Link>
+            </button>
+          </div>
         </div>
       </div>
     </main>
