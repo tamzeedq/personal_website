7d933cf85487a39b799f9dafba843dec32d89865
tamzeedq <74509993+tamzeedq@users.noreply.github.com>
2023-09-20 21:32:30 -0700

    update about page and fix styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className='max-h-screen flex'>
+    <div className='min-h-screen flex'>
       <div className='w-[50%] max-h-[100%]'>
         <img className='w-full h-full object-cover' src="/images/tamzeed/alley_pic.jpg" alt="Tamzeed Photo" />
       </div>
       
-      <div className='w-[50%] max-h-[100%] p-16 overflow-y-scroll no-scrollbar text-white'>
-        <h1 className='text-7xl'>About Me</h1>
+      <div className='w-[50%] max-h-[100%] py-16 px-8 overflow-y-scroll no-scrollbar text-white'>
+        <h1 className='text-3xl sm:text-4xl md:text-7xl'>About Me</h1>
         <hr />
         <br />
-        <p>Hi! My name is Tamzeed Quazi. I am in my third year at the University of British Columbia pursuing a combined major in Computer Science and Statistics. Currently, I am a front-end developer for UBC MINT, and a full-stack developer and software engineer at my co-op. In the future, I hope to pursue a career in machine learning.</p>
+        <p>
+          Hi! My name is Tamzeed Quazi. I am in my third year at the University of British Columbia pursuing a combined major in Computer Science and Statistics. 
+          I am a developer for UBC Solar's Simulation Team, and have just completed a co-op as a full-stack developer and software engineer at Tetra Tech. <br /> <br />
+          My interests include machine learning, computer vision, computer graphics, and front-end development. Lower-level programming and C are some skills I am currently picking up.
+        </p>
         <br />
         <p>I enjoy watching movies, playing various sports, going to the gym, playing chess, gaming, and lately have been tinkering with my new Arduino starter kit. Check out my resume to see more of the professional experience I have!</p>
         <br />
-        <p>Feel free to check out my socials and get in touch!</p>
+        <p>Feel free to check out my socials and get in touch! <br />I am currently looking for internship opportunities for Summer 2024!</p>
       </div>
     </div>
   );
