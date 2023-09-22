a5aa9b451dea2114141abf2479f52527c6401441
tamzeedq <74509993+tamzeedq@users.noreply.github.com>
2023-09-21 21:36:53 -0700

    fixed build issue with about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,17 +9,21 @@ const About = () => {
       
       <div className='w-[50%] max-h-[100%] py-16 px-8 overflow-y-scroll no-scrollbar text-white'>
         <h1 className='text-3xl sm:text-4xl md:text-7xl'>About Me</h1>
-        <hr />
+        <hr className="border border-red-400"/>
         <br />
         <p>
-          Hi! My name is Tamzeed Quazi. I am in my third year at the University of British Columbia pursuing a combined major in Computer Science and Statistics. 
-          I am a developer for UBC Solar's Simulation Team, and have just completed a co-op as a full-stack developer and software engineer at Tetra Tech. <br /> <br />
+          Hi! My name is Tamzeed Quazi. I am in my third year at the University of British Columbia pursuing a combined major in Computer Science and Statistics.
+          {" I am a developer for UBC Solar's Simulation Team, and have just completed a co-op as a full-stack developer and software engineer at Tetra Tech."} 
+        </p>
+        <br />
+        <p>
           My interests include machine learning, computer vision, computer graphics, and front-end development. Lower-level programming and C are some skills I am currently picking up.
         </p>
         <br />
         <p>I enjoy watching movies, playing various sports, going to the gym, playing chess, gaming, and lately have been tinkering with my new Arduino starter kit. Check out my resume to see more of the professional experience I have!</p>
         <br />
-        <p>Feel free to check out my socials and get in touch! <br />I am currently looking for internship opportunities for Summer 2024!</p>
+        <p>Feel free to check out my socials and get in touch!</p>
+        <p className='text-red-300'>I am currently looking for internship opportunities for Summer 2024!</p>
       </div>
     </div>
   );
